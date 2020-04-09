8806d1f65ef92e41b4f81c3d519ef68fe4acb0c7
DouglasHdezT <00004516@uca.edu.sv>
2020-04-09 14:28:13 -0600

    BreakPoint of Death

--- store/Store.js
+++ store/Store.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import BudgetReducer from './reducers/BudgetReducer'
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
 	budget: BudgetReducer,
 });
 
-const mainStore = createStore(rootReducer);
+const mainStore = createStore(rootReducer, applyMiddleware(thunk));
 
 export default mainStore;
