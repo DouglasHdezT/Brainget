a607c832d78bb857be24afa36e0c9e863303fdaf
DouglasHdezT <00004516@uca.edu.sv>
2020-04-12 16:25:21 -0600

    Loading settings

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { Provider } from 'react-redux';
+import { AsyncStorage } from 'react-native'
 
 import store from './store/Store'
+
+import { PERIODS_KEY } from './assets/constants/KeyValues';
+import { periodsSettings } from './components/alerts/Alerts';
 
 import MainNavigationStack from './components/navigations/MainNavigationStack'
 
@@ -36,18 +40,16 @@ const loadData = async () => {
 		console.log("Fuentes Cargadas");
 		
 		//Configuración básica
-		let periodsConfig = await AsyncStorage.getItem(keys.periodsConfigKey);
+		let periodsConfig = await AsyncStorage.getItem(PERIODS_KEY);
 
 		if (periodsConfig == null){
 			periodsConfig = await periodsSettings()
-			AsyncStorage.setItem(keys.periodsConfigKey, periodsConfig)
+			await AsyncStorage.setItem(PERIODS_KEY, periodsConfig)
 		}
 		
-		console.log("Configuraciones cargadas");
-
-		resolve();
+		console.log(`Configuraciones cargadas ${periodsConfig}`);
 
 	} catch(err) {
-		reject(err);
+		console.log(err)
 	}
 }
