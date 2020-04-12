2ac297187c293f3ddea9bd8b0d2222658f9a063a
DouglasHdezT <00004516@uca.edu.sv>
2020-04-12 16:32:21 -0600

    Add: Date utils

---
+++ utils/DateUtils.js
@@ -0,0 +1,50 @@
+/**
+ * Convencon de uso
+ * Years => 2020, de forma normal
+ * Month - 0 - 12 => Enero = 0, Diciembre = 11
+ * Day - 1 - 31 
+ */
+
+export const getLastDayInMonth = (month, year) => {
+	return new Date(year, month + 1, 0).getDate();
+}
+
+export const dateInObject = () => {
+	const date = new Date();
+	const day = date.getDate();
+	const month = date.getMonth();
+	const year = date.getFullYear();
+
+	return { day, month, year };
+}
+
+export const getPeriodLimits = (periods) => {
+	const date = new Date();
+	const day = date.getDate();
+	const month = date.getMonth();
+	const year = date.getFullYear();
+
+	const lastDay = getLastDayInMonth(month, year);
+
+	const jump = Math.ceil(lastDay / periods);
+
+	let limitBot = 0
+	let limitTop = limitBot + jump;
+
+	for ( let i = 0 ; i < periods ; i++ ) {
+		if(day > limitBot && day <= limitTop){
+			break;
+		}
+		limitBot += jump
+		limitTop += jump
+		limitTop = limitTop <= lastDay ? limitTop : lastDay;
+
+	} 
+
+	limitTop +=1;
+
+	return {
+		startDay: limitBot,
+		endDay: limitTop
+	}
+}
